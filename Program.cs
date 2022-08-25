3361d5c860c3a8f442edbfddb4ef5b8cb4cc3f06
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-25 21:16:43 +0530

    created a binarysearchtree program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BinarySearchTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Hash Table and Binary Search Tree");
+
+            BinaraySearchTree<int> binaraySearchTree = new BinaraySearchTree<int>
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+
+            binarySearch.Display();
+        }
+    }
+}
